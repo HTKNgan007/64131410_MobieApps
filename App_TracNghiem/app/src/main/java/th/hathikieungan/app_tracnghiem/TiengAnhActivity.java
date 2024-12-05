e5a1d3d4ca806e39f531736ecf8a54d6236c044f
HTKNgan007 <nganha2004nh@gmail.com>
2024-12-05 19:50:41 +0700

    act E

--- App_TracNghiem/app/src/main/java/th/hathikieungan/app_tracnghiem/TiengAnhActivity.java
+++ App_TracNghiem/app/src/main/java/th/hathikieungan/app_tracnghiem/TiengAnhActivity.java
@@ -1,58 +1,34 @@
 package th.hathikieungan.app_tracnghiem;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 public class TiengAnhActivity extends AppCompatActivity{
-    private int currentFragmentIndex = 0;
-    private final Fragment[] fragments = {new E1_Fragment(), new E2_Fragment(), new E3_Fragment()};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tieng_anh);
 
-        // Hiển thị fragment đầu tiên
-        replaceFragment(fragments[currentFragmentIndex]);
+        // Gắn Fragment 1 vào fragment_container1
+        E1_Fragment fragment1 = new E1_Fragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentE1, fragment1)
+                .commit();
 
-        // Ánh xạ các nút
-        Button buttonLeft = findViewById(R.id.btn_prev);
-        Button buttonRight = findViewById(R.id.btn_next);
+        // Gắn Fragment 2 vào fragment_container2
+        E2_Fragment fragment2 = new E2_Fragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentE2, fragment2)
+                .commit();
 
-        // Xử lý sự kiện nút Trái
-        buttonLeft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (currentFragmentIndex > 0) {
-                    currentFragmentIndex--;
-                    replaceFragment(fragments[currentFragmentIndex]);
-                }
-            }
-        });
-
-        // Xử lý sự kiện nút Phải
-        buttonRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (currentFragmentIndex < fragments.length - 1) {
-                    currentFragmentIndex++;
-                    replaceFragment(fragments[currentFragmentIndex]);
-                }
-            }
-        });
+        // Gắn Fragment 2 vào fragment_container2
+        E3_Fragment fragment3 = new E3_Fragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentE3, fragment3)
+                .commit();
     }
-
-    // Hàm thay thế fragment
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.fragment_container, fragment);
-        transaction.commit();
-    }
-
 }
