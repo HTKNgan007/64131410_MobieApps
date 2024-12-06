fe79ae6fc5751230e73541cf55f36c3168e06f69
HTKNgan007 <nganha2004nh@gmail.com>
2024-12-06 11:31:40 +0700

    TH bai 4

---
+++ TH_Bai4_BotNav/app/src/main/java/th/hathikieungan/th_bai4_botnav/ui/CN4/CN4_Fragment.java
@@ -0,0 +1,20 @@
+package th.hathikieungan.th_bai4_botnav.ui.CN4;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import th.hathikieungan.th_bai4_botnav.R;
+public class CN4_Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cn4, container, false);
+    }
+}
